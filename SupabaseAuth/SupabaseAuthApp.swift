33b8fda23dc5aee6660c57fd754fa82900f75b8f
David Acosta <dacostacsit@gmail.com>
2023-09-27 20:07:25 -0400

    Initial Commit

---
+++ SupabaseAuth/SupabaseAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupabaseAuthApp.swift
+//  SupabaseAuth
+//
+//  Created by David Acosta on 9/27/23.
+//
+
+import SwiftUI
+
+@main
+struct SupabaseAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
